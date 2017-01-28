f9b13a96a7a44cad7e283c5aefa9bef7fe5a5495
Llikdowk <guillermofm7@gmail.com>
2017-01-28 13:07:53 +0100

    add googletest submodule

--- src/EntryPoint.cpp
+++ src/EntryPoint.cpp
@@ -7,7 +7,8 @@ int main() {
     scene.addObject(new Sphere(0, 0, -2, 1))
          .addObject(new Sphere(0, 1, -2, 0.5f));
 
-    Raytracer<640, 480> raytracer(scene, 90);
-    raytracer.run();
+    //Raytracer<1000, 1000> raytracer(scene, 90);
+    Raytracer<1000, 1000>* raytracer = new Raytracer<1000, 1000>(scene, 90);
+    raytracer->run();
     return 0;
 }

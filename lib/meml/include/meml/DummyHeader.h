3f2169e5ffb2a4ee5acde8c6e84a0a3713b96320
Llikdowk <guillermofm7@gmail.com>
2017-01-29 20:38:28 +0100

    delete meml

--- lib/meml/include/meml/DummyHeader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-namespace meml {
-    void dummyFunction() {
-        std::cout<<"hi"<<std::endl;
-    }
-}
--- lib/meml/tests/SanityCheck.cpp
+++
@@ -1,5 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST (suite_1, test_1) {
-    EXPECT_EQ(1, 1);
-}
